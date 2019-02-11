cadf20f5e764f20f8501b031b56e04e24ea66897
Quico Orozco <esi@plumbaria.com>
2019-02-11 14:26:44 +0100

    Commit terminado Ejercicio 8.9. Servicio que bloquea el hilo principal

--- app/src/main/java/com/plumbaria/e_8_9_intentservice/MainActivity.kt
+++ app/src/main/java/com/plumbaria/e_8_9_intentservice/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.plumbaria.e_8_9_intentservice
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var Salida : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Salida = findViewById(R.id.salida)
+    }
+
+    fun calcularOperacion(view: View){
+        var n : Double = entrada.getText().toString().toDouble()
+        Salida.append(""+n +"^2 = ")
+        var i : Intent = Intent(this, ServicioOperacion::class.java)
+        i.putExtra("numero", n)
+        startService(i)
     }
 }
