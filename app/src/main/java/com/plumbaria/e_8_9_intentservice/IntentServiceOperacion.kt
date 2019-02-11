39b6ba20b7adef00a14bc6ba27e7924cc1b64e56
Quico Orozco <esi@plumbaria.com>
2019-02-11 15:05:10 +0100

    Commit terminado Ejercicio 8.11. Broadcast para la comunicación entre hilos

--- app/src/main/java/com/plumbaria/e_8_9_intentservice/IntentServiceOperacion.kt
+++ app/src/main/java/com/plumbaria/e_8_9_intentservice/IntentServiceOperacion.kt
@@ -10,10 +10,15 @@ import android.os.SystemClock
 
 class IntentServiceOperacion() : IntentService("IntentServiceOperacion") {
 
-    @SuppressLint("WrongThread")
+    //@SuppressLint("WrongThread")
     override fun onHandleIntent(p0: Intent?) {
         var n : Double = p0?.extras?.getDouble("numero")!!
         SystemClock.sleep(5000)
-        MainActivity().Salida.append(""+ n*n + "\n")
+        //MainActivity().Salida.append(""+ n*n + "\n")
+        var i : Intent = Intent()
+        i.setAction(MainActivity().ReceptorOperacion().ACTION_RESP)
+        i.addCategory(Intent.CATEGORY_DEFAULT)
+        i.putExtra("resultado",n*n)
+        sendBroadcast(i)
     }
 }
--- app/src/main/java/com/plumbaria/e_8_9_intentservice/MainActivity.kt
+++ app/src/main/java/com/plumbaria/e_8_9_intentservice/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.plumbaria.e_8_9_intentservice
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,6 +18,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Salida = findViewById(R.id.salida)
+        var filtro : IntentFilter = IntentFilter(ReceptorOperacion().ACTION_RESP)
+        filtro.addCategory(Intent.CATEGORY_DEFAULT)
+        registerReceiver(ReceptorOperacion(),filtro)
     }
 
     fun calcularOperacion(view: View){
@@ -24,6 +30,14 @@ class MainActivity : AppCompatActivity() {
         var i : Intent = Intent(this, IntentServiceOperacion::class.java)
         i.putExtra("numero", n)
         startService(i)
+    }
+
+    inner class ReceptorOperacion : BroadcastReceiver() {
+        val ACTION_RESP : String ="com.plumbaria.e_8_9_intentservice"
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            var res = intent.getDoubleExtra("resultado",0.0)
+            Salida.append(" "+ res)
+        }
     }
 }
 
